dfe91d4090ab0b3c12ebbaf6e0bafc18e85fe1ef
Francis Lagares <francis.lagares@gmail.com>
2022-12-21 20:31:21 +0100

    chore: path aliases setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: {
+            screens: './src/screens',
+            components: './src/components',
+          },
+        },
+      ],
+    ],
   };
 };
